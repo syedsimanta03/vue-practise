0bbcab774ae71c9c88d8444bccce5055ecaafd22
sam <syed.simanta06@gmail.com>
2021-03-13 00:08:07 +0600

    first commit

---
+++ src/components/TheButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-link to="/about">
+  <button class="btn" :class="{'btn-primary': primary, 'btn-danger': danger}">Sign in</button>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    primary: {
+      type: Boolean,
+      default: true
+    },
+    danger: {
+      type: Boolean,
+      default: false
+    },
+  },
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TheButton/>
+    <TheButton danger/>
+  </div>
+</template>
+
+<script>
+import TheButton from '../components/TheButton'
+
+export default {
+  name: 'Home',
+  components: {
+    TheButton,
+  },
+}
+</script>
