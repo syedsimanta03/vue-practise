4ddf311219583fc9face07a0acc618cda7d0be27
sam <syed.simanta06@gmail.com>
2021-03-17 02:15:00 +0600

    slots

--- src/App.vue
+++ src/App.vue
@@ -1,145 +1,45 @@
 <template>
-  <section>
-    <header>
-      <h1>My Friends</h1>
-    </header>
-    <Newfriend @add-contact="addContact" />
-    <ul v-for="friend in friends" :key="friend.id">
-      <FriendContact
-        :name="friend.name"
-        :phoneX="friend.phone"
-        :email="friend.email"
-        :id="friend.id"
-        :isfav="friend.isfav"
-        @toggle-fav="toggleFavStatus"
-        @delete="deleteContact"
-      />
-    </ul>
-  </section>
+  <div>
+    <the-header></the-header>
+    <!-- <TheHeader /> -->
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
 </template>
 
 <script>
-import FriendContact from './components/FriendContact'
-import Newfriend from './components/Newfriend'
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 
 export default {
   components: {
-    FriendContact,
-    Newfriend,
+    TheHeader,
+    BadgeList,
+    UserInfo
   },
   data() {
     return {
-      message: '',
-      friends: [
-        {
-          id: 'manuel',
-          name: 'Manuel Lorenz',
-          phone: '0123 45678 90',
-          email: 'manuel@localhost.com',
-          isfav: true,
-        },
-        {
-          id: 'julie',
-          name: 'Julie Jones',
-          phone: '0987 654421 21',
-          email: 'julie@localhost.com',
-          isfav: false,
-        },
-      ],
-    }
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
   },
-
-  methods: {
-    toggleFavStatus(friendId) {
-      const indentifiedfriend = this.friends.find((f) => f.id === friendId)
-      indentifiedfriend.isfav = !indentifiedfriend.isfav
-    },
-    addContact(name, phone, email) {
-      const newFriendContact = {
-        id: new Date().toISOString(),
-        name: name,
-        phone: phone,
-        email: email,
-        isFavourite: false,
-      }
-      this.friends.push(newFriendContact)
-    },
-
-    deleteContact(friendId) {
-      this.friends = this.friends.filter((f) => f.id !== friendId)
-    },
-  },
-}
+};
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-}
 html {
-  font-family: 'Jost', sans-serif;
+  font-family: sans-serif;
 }
+
 body {
   margin: 0;
-}
-header {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-#app ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-#app li,
-#app form {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  text-align: center;
-  width: 90%;
-  max-width: 40rem;
-}
-#app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
-}
-#app button {
-  font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
-  color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-}
-#app button:hover,
-#app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
-}
-#app input {
-  font: inherit;
-  padding: 0.15rem;
-}
-#app label {
-  font-weight: bold;
-  margin-right: 1rem;
-  width: 7rem;
-  display: inline-block;
-}
-#app form div {
-  margin: 1rem 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import FriendContact from './components/FriendContact.vue';
+import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from './components/BaseCard.vue';
 
 const app = createApp(App);
 
-app.component('friend-contact', FriendContact);
+app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
 
 app.mount('#app');
