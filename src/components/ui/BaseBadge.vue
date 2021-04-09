3359a3cf9451dd6d91f213457fd779b2b6bf99fa
sam <syed.simanta06@gmail.com>
2021-04-09 12:37:29 +0600

    'coach'

--- src/components/ui/BaseBadge.vue
+++ src/components/ui/BaseBadge.vue
@@ -1,36 +1,42 @@
 <template>
-  <span class="badge" :class="['badge--' + mode, {nml: noMarginLeft}]">
-    <slot></slot>
+  <span class="badge" :class="type">
+    {{ text }}
   </span>
 </template>
 
 <script>
 export default {
-  props: ['mode', 'noMarginLeft']
+  props: ['type', 'title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    }
+  }
 }
 </script>
 
 <style scoped>
 .badge {
-  display: inline-block;
-  padding: 0.15rem 1.25rem;
   background-color: #ccc;
-  color: #292929;
-  margin: 0 0.75rem;
+  color: #252525;
   border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  margin-right: 0.5rem;
 }
 
-.nml {
-  margin-left: 0;
+.frontend {
+  background-color: #3d008d;
+  color: white;
 }
 
-.badge--highlight {
-  background-color: #f0b800;
-  color: black;
+.backend {
+  background-color: #71008d;
+  color: white;
 }
 
-.badge--elegant {
-  background-color: #45006d;
+.career {
+  background-color: #8d006e;
   color: white;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,15 @@
-import { createStore } from 'vuex'
-import productsModule from './modules/products.js'
-import cartModule from './modules/cart'
+import { createStore } from 'vuex';
+
+import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js';
 
 const store = createStore({
-  state() {
-    return {
-      isLoggedIn: false,
-    }
-  },
-  //@params: state, payload
-  //modify state
-  mutations: {
-    login(state) {
-      state.isLoggedIn = true
-    },
-    logout(state) {
-      state.isLoggedIn = false
-    },
-  },
+  modules: {
+    coaches: coachesModule,
+    requests: requestsModule,
+    auth: authModule
+  }
+});
 
-  //@params: context, payload
-  //components uses specific action -> to send data as a payload to add into mutation param -> to change .state
-  actions: {
-    login(context) {
-      context.commit('login') //commit('mutationName')
-    },
-    logout(context) {
-      context.commit('logout') //commit('mutationName')
-    },
-  },
-  //@params: state, getters
-  //modify state
-  //  getters pulls current state
-  getters: {
-    isAuthenticated(state) {
-      return state.isLoggedIn
-    },
-  },
-
-  modules: { productsModule, cartModule },
-})
-
-export default store
+export default store;
