dad708613172b02b9a5df8b520c4d2a654e5f1da
sam <syed.simanta06@gmail.com>
2021-04-11 00:13:08 +0600

    router

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,46 @@
 <template>
-  <section class="container">
-    <user-data :first-name="firstName" :last-name="lastName" :age="age"></user-data>
-    <button @click="setAge">Change Age</button>
-    <div>
-      <input type="text" placeholder="First Name" v-model="firstName" />
-      <input type="text" placeholder="Last Name" ref="lastNameInput" />
-      <button @click="setLastName">Set Last Name</button>
-    </div>
-  </section>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <script>
-import { ref, computed, watch } from 'vue';
-import UserData from './components/UserData.vue';
+import { ref, provide } from 'vue';
+
+import TheHeader from './components/TheHeader.vue';
+
 export default {
   components: {
-    UserData
+    TheHeader,
   },
   setup() {
-    // const uName = ref('Maximilian');
-    const firstName = ref('');
-    const lastName = ref('');
-    const lastNameInput = ref(null);
-    const uAge = ref(31);
-    // const user = reactive({
-    //   name: 'Maximilian',
-    //   age: 31,
-    // });
-    const uName = computed(function() {
-      return firstName.value + ' ' + lastName.value;
-    });
-     watch([uAge, uName], function(newValues, oldValues) {
-      console.log('Old age: ' + oldValues[0]);
-      console.log('New age: ' + newValues[0]);
-      console.log('Old name: ' + oldValues[1]);
-      console.log('New name: ' + newValues[1]);
-    });
-    function setNewAge() {
-      uAge.value = 33;
+    const products = ref([
+      {
+        id: 'p1',
+        title: 'A Carpet',
+        description: 'A nice looking, maybe a little bit used carpet.',
+        price: 15.99,
+      },
+      {
+        id: 'p2',
+        title: 'A Book',
+        description: 'You can read it. Maybe you should read it.',
+        price: 12.99,
+      },
+    ]);
+
+    function addProduct(productData) {
+      const newProduct = {
+        id: new Date().toISOString(),
+        title: productData.title,
+        description: productData.description,
+        price: productData.price,
+      };
+      products.value.push(newProduct);
     }
-    function setLastName() {
-      lastName.value = lastNameInput.value.value;
-    }
-    return {
-      userName: uName,
-      age: uAge,
-      setAge: setNewAge,
-      firstName,
-      lastName,
-      lastNameInput,
-      setLastName
-    };
+
+    provide('products', products);
+    provide('addProduct', addProduct);
   },
-  // data() {
-  //   return {
-  //     userName: 'Maximilian',
-  //     age: 31
-  //   };
-  // },
-  // methods: {
-  //   setNewAge() {
-  //     this.age = 32;
-  //   }
-  // },
-  // watch: {
-  //   age(val) {
-  //     console.log(val);
-  //   }
-  // }
 };
 </script>
 
@@ -75,12 +48,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 .container {
   margin: 3rem auto;
   max-width: 30rem;
